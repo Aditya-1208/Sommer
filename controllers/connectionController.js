d134dc064932a763f7cfa41fa04e03646fe4dd83
Aditya-1208 <adityaagr.201ec103@nitk.edu.in>
2022-10-02 17:48:24 +0530

    connections refactoring

---
+++ controllers/connectionController.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const app = require(`${__dirname}/../app.js`);
+exports.connectDb = async () => {
+    const connectionURL = `mongodb+srv://aditya:${process.env.PASSWORD}@cluster0.grdy9dc.mongodb.net/?retryWrites=true&w=majority`
+    try {
+        const connection = await mongoose.connect(connectionURL);
+        console.log('Database connected successsfully');
+        return connection;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.runServer = async () => {
+    try {
+
+        const port = process.env.PORT || 3000;
+        await app.listen(port)
+        console.log(`Server Listening on port ${port}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-const mongoose = require('mongoose');
-const app = require(`${__dirname}/app.js`);
 const dotenv = require('dotenv');
-
 dotenv.config({ path: `${__dirname}/config.env` });
+const app = require(`${__dirname}/app.js`);
+const { connectDb, runServer } = require(`${__dirname}/controllers/connectionController`);
 
-const connectionURL = `mongodb+srv://aditya:${process.env.PASSWORD}@cluster0.grdy9dc.mongodb.net/?retryWrites=true&w=majority`
-
-mongoose.connect(connectionURL).then(() => {
-    console.log('Database connected successfully');
-}).catch(err => console.log(err));
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server Listening on port ${port}`);
-})
+connectDb();
+runServer();
