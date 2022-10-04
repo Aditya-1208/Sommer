a4ae9b9e0fc0b28b2ac32afd3a64c3fefa9006ca
Aditya-1208 <adityaagr.201ec103@nitk.edu.in>
2022-10-04 10:40:15 +0530

    signup sending cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,13 +2,18 @@ const catchAsync = require(`${__dirname}/../utils/catchAsync.js`);
 const userModel = require(`${__dirname}/../models/userModel.js`);
 
 exports.signup = catchAsync(async (req, res, next) => {
+    //run validators using suitable package or yourself!
     const newUser = await userModel.create(req.body);
     newUser.password = undefined;
     const jwt = newUser.signJWT();
-    res.status(201).json({
+    //set cookie to httpOnly and secure in prod
+    const cookieOptions = {
+        maxAge: 30 * 24 * 60 * 60 * 1000 //30 days
+    }
+    res.cookie('jwt', jwt, cookieOptions).status(201).json({
         res: "success",
         data: {
-            jwt, newUser
+            newUser
         }
     })
     next();
